360ca6bdbc23e81a479bc510f1fb2c2aea6c86c5
AmirEnzeva <amirezaabdolahi752@gmail.com>
2025-01-21 17:57:54 +0330

    some changeas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
-let expertiseSwiper = new Swiper('.expertise-swiper',{
-    direction: 'vertical',
-    loop: true,
-    autoplay: {
-        delay: 3000,
-    },
+const swiper = new Swiper('.swiper', {
+    speed: 1000,
+    spaceBetween: 100,
+    loop:true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        980: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+      },
 
-})
+      autoplay: {
+        delay: 1000,
+      },
+      
+      
+  });
