4ae7403f80e234255c068c4eff870a839e271bea
AmirEnzeva <amirezaabdolahi752@gmail.com>
2025-01-23 13:31:32 +0330

    some changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,37 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.expertise-swiper', {
+    speed: 1000,
+    spaceBetween: 100,
+    loop:true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        980: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+      },
+
+      autoplay: {
+        delay: 1000,
+      },
+      
+      
+  });
+
+const doctorSwiper = new Swiper('.doctor-swiper', {
     speed: 1000,
     spaceBetween: 100,
     loop:true,
