b855825c6e1e23b599cb2006dbd26b44d01dcd33
AmirEnzeva <amirezaabdolahi752@gmail.com>
2025-02-15 22:05:00 +0330

    some chages

--- js/shared.js
+++ js/shared.js
@@ -8,13 +8,16 @@ let menu = $.getElementById('menu')
 let menuHandler = ()=>{
     if (!menu.classList.contains('menu--active')) {
         menu.classList.add('menu--active')
-        Body.classList.add('over-flow-hidden')
     }else{
         menu.classList.remove('menu--active')
-        Body.classList.remove('over-flow-hidden')
     }
+    
+}
 
+function closeHandler(){
+    
 }
 
 openMenuBtn.addEventListener('click',menuHandler)
 closeMenuBtn.addEventListener('click',menuHandler)
+window.addEventListener('scroll',() => menu.classList.remove('menu--active'))
