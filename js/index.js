a7f4a3ae7cfadb3d4e5bd1e76ba4fea2901a2884
AmirEnzeva <amirezaabdolahi752@gmail.com>
2025-03-15 09:36:14 +0330

    some changes

--- js/index.js
+++ js/index.js
@@ -1,180 +1,23 @@
-const expertiseSwiper = new Swiper('.swiper-1', {
-  speed: 1000,
-  spaceBetween: 100,
+const swiper = new Swiper('.swiper', {
   loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 0,
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      }
+      slidesPerView: 2,
     },
     // when window width is >= 480px
     480: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    },
-    // when window width is >= 640px
-    980: {
       slidesPerView: 3,
-      spaceBetween: 40,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    },
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 0,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    }
-  },
-
-  autoplay: {
-    delay: 1000
-  }
-
-});
-
-const doctorSwiper = new Swiper('.doctor-swiper', {
-  speed: 1000,
-  spaceBetween: 100,
-  loop: true,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-
     },
     // when window width is >= 640px
-    980: {
-      slidesPerView: 3,
-      spaceBetween: 40
-    },
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    }
-  },
-
-  autoplay: {
-    delay: 1000,
-    reverseDirection: true,
-  },
-
-
-});
-
-const illSignSwiper = new Swiper('.swiper-2', {
-  speed: 1000,
-  spaceBetween: 100,
-  loop: true,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    },
-    // when window width is >= 640px
-    980: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    },
-    1200: {
+    998: {
       slidesPerView: 6,
-      spaceBetween: 0,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
     }
-  },
-
-  autoplay: {
-    delay: 1000,
-  }
-
-});
-const commentsSwiper = new Swiper('.swiper-3', {
-  speed: 1000,
-  spaceBetween: 100,
-  loop: true,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    },
-    // when window width is >= 640px
-    980: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    },
-    1200: {
-      slidesPerView: 6,
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-    }
-  },
-
-  autoplay: {
-    delay: 3000,
   }
 
 });
